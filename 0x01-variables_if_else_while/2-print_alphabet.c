104a9147dde7c22d58de6be2bdc484f441704101
TUTUDAVE <tutudavid751@gmail.com>
2022-12-10 07:50:43 -0800

    writing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for printing lowercase letters
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	putchar("Lowercase letters: \n");
+
+	for (ch = 'a'; ch <= 'z'' ch++)
+	{
+		putchar ("%c ", ch);
+	}
+
+	return (0);
+}
