1b30a8cc5453fee807c1ec757abb6da4e7064dc4
TUTUDAVE <tutudavid751@gmail.com>
2022-12-11 18:13:15 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -printing numbers of base 10 starting from 0 using putchar
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char (int k);
+
+	for (int k = 0; int k < 10; k++)
+	{
+		putchar(int k);
+	}
+
+	putchar('\n');
+
+}
