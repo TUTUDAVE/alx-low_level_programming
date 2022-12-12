bedfb4e7dfe99c61a16e41757d1417f7c2053318
TUTUDAVE <tutudavid751@gmail.com>
2022-12-12 13:49:35 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -printing lowecase alphabets in the reverse order
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch = <= 'a'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
