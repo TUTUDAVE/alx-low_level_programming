ca4a26e823a2128097e0cff9d99c6e435e86107a
TUTUDAVE <tutudavid751@gmail.com>
2022-12-19 14:57:27 -0800

    a script to return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+/**
+ * _strlen-Finds the length of a string.
+ *
+ * @s: String to a pointer whose length is to be determined.
+ *
+ * Return: 0 (success)
+ */
+
+int _strlen(char *s)
+{
+	int p = 0;
+
+	while (*(s + p) != 0) /*increament until last character not up to zero*/
+	{
+		p++;
+	}
+
+	return (p);
+}
